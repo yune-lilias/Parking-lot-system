152ee4053b5045e633acef2f1a18bce13c4f90c3
Steven Maharath <77238935+smaharath1@users.noreply.github.com>
2021-04-29 16:16:29 -0400

    Posts ot itself

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en-US">
     <head>
@@ -6,9 +7,23 @@
         <link rel="stylesheet" href="main.css">
     </head>
     <body>
+    <?php
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    if(isset($_POST["name"]) && ($_POST['name'] != "") && isset($_POST["password"]) && ($_POST['password'] != "")){
+        $name = $_POST["name"];
+        $password = $_POST["password"];
+        
+    }else{
+        echo '<script language="javascript">';
+        echo 'alert("Login Failed")';
+        echo '</script>';
+    }
+}
+?>
         <div id="box">
-            <form action="" method="post">
-            <p>Username: <input id="name" type="text"></p>
+            <form action="login.php" method="post">
+            <p>Username: <input id="name" type="text" name="name"></p>
 		    <p>Password: <input name="password" type="password"></p>
             <input type="submit" id="" value="Login">
             </form>
