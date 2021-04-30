9f368821d9e0be3f8192e257a42754321d4ed148
Steven Maharath <77238935+smaharath1@users.noreply.github.com>
2021-04-30 13:04:14 -0400

    Main creates a database on startup

--- main.php
+++ main.php
@@ -6,6 +6,26 @@
         <link rel="stylesheet" href="main.css">
     </head>
     <body>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "mydb";
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    
+    $sql = "CREATE DATABASE mydb";
+    if (mysqli_query($conn, $sql)) {
+        echo "Database created successfully<br>";
+    } else {
+        echo "Error creating database: " . mysqli_error($conn) . "<br>";
+    }
+    mysqli_select_db ( $conn , "mydb" );
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }    
+    ?>
         
     <div id="main">
         <div id="banner-r">
