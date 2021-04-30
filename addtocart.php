b037a92f7cf7f9138a988907268e7f298dd5fb63
Steven Maharath <77238935+smaharath1@users.noreply.github.com>
2021-04-29 21:42:28 -0400

    Add files via upload

--- addtocart.php
+++ addtocart.php
@@ -4,7 +4,19 @@
         <meta charset="utf-8">
     </head>
     <body>
-
+        <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "mydb";
+            // Create connection
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);      
+            }    
+            //Once done go to viewcart.php
+        ?>
     </body>
 
 </html>
--- inventory.php
+++ inventory.php
@@ -6,15 +6,32 @@
         <link rel="stylesheet" href="main.css">
     </head>
     <body>
+        <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "mydb";
+            // Create connection
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+             // Check connection
+             if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }    
+        ?>
     <div id="navbar">
-            <ul>
-                <li><a href="menu.php">Home</a></li>
-                <li><a href="profile.php">My Profile</a></li>
-                <li><a href="invetory.php">Rent a Car</a></li>
-                <li><a href="">Pre-Pay for Parking</a></li>
-                <li><a href="viewcart.php">My Cart</a></li>
-            </ul>
-        </div>
+        <ul>
+            <li><a href="menu.php">Home</a></li>
+            <li><a href="profile.php">My Profile</a></li>
+            <li><a href="inventory.php">Rent a Car</a></li>
+            <li><a href="parking.php">Pre-Pay for Parking</a></li>
+            <li><a href="viewcart.php">My Cart</a></li>
+        </ul>
+    </div>
+    <div>
+        <form action="addtocart.php" method="post">
+
+        </form>
+    </div>
     </body>
 
 </html>
