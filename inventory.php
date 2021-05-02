907d73147ec8dec94ff50c59a09516eeebb8bc19
Steven Maharath <77238935+smaharath1@users.noreply.github.com>
2021-05-01 23:31:59 -0400

    Included model.php

--- inventory.php
+++ inventory.php
@@ -3,23 +3,11 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Car Invetory</title>
+    <title>Car Inventory</title>
     <link rel="stylesheet" href="main.css">
 </head>
 
 <body>
-    <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "mydb";
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    ?>
     <div id="navbar">
         <ul>
             <li><a href="menu.php">Home</a></li>
@@ -29,43 +17,55 @@
             <li><a href="viewcart.php">My Cart</a></li>
         </ul>
     </div>
-    <div>
-        <?php
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "mydb";
-        // Create connection
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        
-        $sql = "SELECT * FROM cars";
-        $result = $conn->query($sql);
-        echo "<h3>Cars</h3>";
-        if ($result->num_rows > 0) {
-            // output data of each row
-            echo "<table><tr><th>Cars</th><th>Type</th><th>Seats</th><th>Cost</th></tr>";
-            while ($row = $result->fetch_assoc()) {
-                $carname = $row["carname"];
-                $cartype = $row["cartype"];
-                $seats = $row["seats"];
-                $price = $row["price"];
+    <div class="flexbox">
+        <div class="inline">
+            <h3>Cars</h3>
+            <?php
+            include 'model.php';
+            $model = new model();
 
+            $sql = "SELECT * FROM Cars";
+            $result = $model->sqlcommend($sql);
 
-                echo "<tr><td>" . $carname . "</td><td>" . $cartype . "</td><td>" . $seats . "</td><td>" . $price . "</td></tr>";
+            $carname = [];
+            $carprice = [];
+            $carid = [];
+            if (!empty($result)) {
+                // output data of each row
+
+                echo "<table class='table-inline'><tr><th>Car Name</th><th>Type</th><th>Seats</th><th>Price</th></tr>";
+                foreach ($result as $key => $value) {
+                    $row = $value;
+                    $id = $row["cars_id"];
+                    $car = $row["carname"];
+                    $type = $row["cartype"];
+                    $seats = $row["seats"];
+                    $price = $row["price"];
+                    array_push($carname, $car);
+                    array_push($carprice, $price);
+                    array_push($carid, $id);
+                    echo "<tr><td>" . $car . "</td><td>" . $type . "</td><td>" . $seats . "</td><td>" . $price . "</td></tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "0 results";
             }
-            echo "</table>";
-        } else {
-            echo "0 results";
-        }
-        $conn->close();
-        ?>
-        <form action="addtocart.php" method="post">
-
-        </form>
+            ?>
+        </div>
+        <div class="inline">
+        <h2>Choose a Car</h2>
+            <form action="addtocart.php" method="post">
+                <?php
+                echo "<label for='car'>Car Name</label>";
+                echo "<select id='car' name='car'>";
+                for ($i = 0; $i < sizeof($carname); $i++) {
+                    echo "<option value='$carname[$i]'>$carname[$i]</option>";
+                }
+                echo "</select>";
+                ?>
+                 <input type="submit" value="Submit">
+            </form>
+        </div>
     </div>
 </body>
 
